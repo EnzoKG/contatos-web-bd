72535f30118c48b7c87163a6ba580f0925bf6926
EnzoKG <enzogiovanini@gmail.com>
2020-12-14 02:07:31 -0400

    Mexendo cabeçalho e rodapé.

--- contatos-web/public/pages/templates/rodape.php
+++ contatos-web/public/pages/templates/rodape.php
@@ -1,6 +1,6 @@
 <footer>
         <p>
-            <emphasis>Copyright © 2020. Site feito por Breno e Enzo em novembro de 2020</emphasis>
+            <emphasis>Copyright © 2020. Site feito por Breno e Enzo em dezembro de 2020</emphasis>
         </p>
     </footer>
 </body>
